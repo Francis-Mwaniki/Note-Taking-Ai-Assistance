480190ac8e55773b2074b51d629c2db02671c65a
Francis-Mwaniki <francismwaniki630@gmail.com>
2023-10-01 14:21:45 +0300

    Internship reports auto generates

--- lib/reportGenerator.tsx
+++ lib/reportGenerator.tsx
@@ -1,14 +1,59 @@
 // export async function generateTableOfContents(companyName: string, foundingYear: number, internshipTitle: string, projectTitle: string): Promise<string> {
     
 // }
+/*  schoolName,
+            campusName,
+            courseName,
+            studentName,
+            studentId,
+            supervisorName,
+            supervisorEmail,
+            supervisorPhone,
+            startDate,
+            endDate,
+            duration,
+            reportTitle,
+            reportType,
+            reportFormat */
 
-export function generateInternshipReport(companyName: string, foundingYear: number, internshipTitle: string, projectTitle: string, description:string) {
+export function generateInternshipReport(companyName: string, foundingYear: number, internshipTitle: string, projectTitle: string, description:string,
+      schoolName: string,
+      campusName: string,
+      courseName: string,
+      studentName: string,
+      studentId: string,
+      supervisorName: string,
+      supervisorEmail: string,
+      supervisorPhone: string,
+      startDate: string,
+      endDate: string,
+      duration: string,
+      reportTitle: string,
+      reportType: string,
+      reportFormat: string
+
+   ) {
  const data = {
     companyName: companyName,
     foundingYear: foundingYear,
     internshipTitle: internshipTitle,
     projectTitle: projectTitle,
     description: description,
+      schoolName: schoolName,
+      campusName: campusName,
+      courseName: courseName,
+      studentName: studentName,
+      studentId: studentId,
+      supervisorName: supervisorName,
+      supervisorEmail: supervisorEmail,
+      supervisorPhone: supervisorPhone,
+      startDate: startDate,
+      endDate: endDate,
+      duration: duration,
+      reportTitle: reportTitle,
+      reportType: reportType,
+      reportFormat: reportFormat
+      
  }
 
 return data;
